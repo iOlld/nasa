a571328d94878e022cf04303231aa182819a3923
iOlld <11vitamin11@rambler.ru>
2022-06-10 23:27:12 +0300

    base style config

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
+<template>
+  <Default/>
+</template>
+
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Default from '@/layouts/Default.vue'
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/layouts/Default.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="maz-text-2xl maz-text-primary">
+    Lets GO!
+  </h1>
+  <h2 class="text-2xl text-red-500">Test tailwindcss</h2>
+  <maz-btn color="black" size="sm" outline>black</maz-btn>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+
+</style>
