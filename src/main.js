fcad402bb47d5136392454b9ae268e6d4399724a
iOlld <11vitamin11@rambler.ru>
2022-06-10 21:56:41 +0300

    env

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import componentsPlugin from './plugins/components'
-//
+
 // import 'maz-ui/css/main.css'
 import './assets/scss/index.scss'
-
 
 const app = createApp(App)
 	.use(router)
